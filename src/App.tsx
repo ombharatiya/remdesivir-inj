52c58a2a72c371f46689171d76651c982b075bce
ombharatiya <om.bharatiya@verizonmedia.com>
2021-04-18 13:53:04 +0530

    some gliberish text

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          be kind
+          be kind, calm mind
         </p>
         <a
           className="App-link"

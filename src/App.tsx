6c32460d0560596e180c2ea03acf8786f01a33f2
ombharatiya <om.bharatiya@verizonmedia.com>
2021-04-18 05:47:03 +0530

    Added change for test

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Folks
+          be kind
         </p>
         <a
           className="App-link"

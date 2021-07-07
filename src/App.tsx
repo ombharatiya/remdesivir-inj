8c985811199246c13b9467b352a129e6b4681790
ombharatiya <om.bharatiya@verizonmedia.com>
2021-07-07 09:20:51 +0530

    Test commit

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          A Simple Exercise
+          A Simple Exercise for Test
         </p>
         <a
           className="App-link"

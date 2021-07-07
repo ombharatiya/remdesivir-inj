d363d34abaf6a392b1493dd81e28fdfca46d8615
ombharatiya <om.bharatiya@verizonmedia.com>
2021-07-07 09:06:31 +0530

    test ssh

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          A Simple Exercise
         </p>
         <a
           className="App-link"
